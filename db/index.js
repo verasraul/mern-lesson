d446f5279939e4218e6448d12b549c0714cc4437
Raul Veras <verasraul@gmail.com>
2023-06-20 22:58:41 -0400

    updating index module for database

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 // connect to Mongo and create new DB
-let MONGO_URI = process.env.PROD_MONGODB || process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/itemsDatabase'
+let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/itemsDatabase'
 
-mongoose.connect(MONGO_URI, { useUnifiedTopology: true,
-useNewParser: true }).then(() => {
+mongoose.connect(MONGODB_URI, { useUnifiedTopology: true,
+useNewUrlParser: true }).then(() => {
     console.log('Successfully connected to MongoDB')
   }).catch(e => {
     console.error('Connection error', e.message)
